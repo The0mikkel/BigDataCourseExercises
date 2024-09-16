02f73ebc80b52a1f0ddbb2ef92a868a71c66f297
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2024-09-16 10:00:51 +0200

    Update reader implementation

--- lectures/02/powergrid-sampler/app.py
+++ lectures/02/powergrid-sampler/app.py
@@ -112,8 +112,8 @@ def get_health():
 
 @app.route("/data")
 def get_data():
-    df = client.read("/powergrid/electricity_lines/wattage_offset/")
-    # df = client.read_all("/powergrid/electricity_lines/wattage_offset/")
+    # df = client.read("/powergrid/electricity_lines/wattage_offset/")
+    df = client.read_all("/powergrid/electricity_lines/wattage_offset/")
     
     # Sort by created_at, sensor_id and correlation_id
     df = df.sort_values(by=["created_at", "sensor_id", "correlation_id"])
